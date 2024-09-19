ec2a6c568c81501cf68ef6240da0a84c626d4e31
20240729 <yoohyeonsik@gmail.com>
2024-09-19 16:33:14 +0900

    fix: requestmapping

--- src/main/java/com/nbacm/zzap_ki_yo/domain/order/controller/OrderController.java
+++ src/main/java/com/nbacm/zzap_ki_yo/domain/order/controller/OrderController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api/v1")
 public class OrderController {
 
     private final OrderServiceImpl orderServiceImpl;
