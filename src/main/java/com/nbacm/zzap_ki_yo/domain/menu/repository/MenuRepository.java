55dcf1e9be1ecf9b2a1981a69d7a4d8b3337ae53
dam <ekadl025@gmail.com>
2024-09-20 13:19:08 +0900

    git pull 하기 전 커밋

--- src/main/java/com/nbacm/zzap_ki_yo/domain/menu/repository/MenuRepository.java
+++ src/main/java/com/nbacm/zzap_ki_yo/domain/menu/repository/MenuRepository.java
@@ -3,5 +3,7 @@ package com.nbacm.zzap_ki_yo.domain.menu.repository;
 import com.nbacm.zzap_ki_yo.domain.menu.entity.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MenuRepository extends JpaRepository<Menu, Long> {
 }
