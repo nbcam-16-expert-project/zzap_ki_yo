aee094e3b84f3e3f3e350793212090203b9d9c44
KangWookOh <kanguk456@gmail.com>
2024-09-22 10:33:08 +0900

    feat: 어드민 대시보드

--- src/main/java/com/nbacm/zzap_ki_yo/domain/controller/DashBoardController.java
+++ src/main/java/com/nbacm/zzap_ki_yo/domain/controller/DashBoardController.java
@@ -11,14 +11,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+
 @RestController
-@RequestMapping("/api/v1/stores/{storeId}/statistics")
+@RequestMapping("/api/v1/stores")
 @RequiredArgsConstructor
 public class DashBoardController {
     private final AdminStoreServiceImpl adminStoreService;
 
     // 일간 통계 조회 (사장님 또는 관리자)
-    @GetMapping("/daily")
+    @GetMapping("{storeId}/statistics/daily")
     public ResponseEntity<StoreStatisticsResponseDto> getDailyStatistics(
             @PathVariable Long storeId,
             @Auth AuthUser authUser) {
@@ -29,13 +31,32 @@ public class DashBoardController {
     }
 
     // 월간 통계 조회 (사장님 또는 관리자)
-    @GetMapping("/monthly")
+    @GetMapping("{storeId}/statistics/monthly")
     public ResponseEntity<StoreStatisticsResponseDto> getMonthlyStatistics(
             @PathVariable Long storeId,
             @Auth AuthUser authUser) {
 
         // AdminStoreServiceImpl의 getMonthlyStatistics 호출
         StoreStatisticsResponseDto statistics = adminStoreService.getMonthlyStatistics(storeId, authUser.getEmail());
+        return ResponseEntity.ok(statistics);
+    }
+    // 전체 배달 어플리케이션의 일간 통계 조회 (관리자 전용)
+    @GetMapping("/daily-all")
+    public ResponseEntity<StoreStatisticsResponseDto> getDailyAllStatistics(
+            @Auth AuthUser authUser) {
+
+        // 관리자 권한이 필요
+        StoreStatisticsResponseDto statistics = adminStoreService.getDailyAllStatistics(authUser.getEmail());
+        return ResponseEntity.ok(statistics);
+    }
+
+    // 전체 배달 어플리케이션의 월간 통계 조회 (관리자 전용)
+    @GetMapping("/monthly-all")
+    public ResponseEntity<StoreStatisticsResponseDto> getMonthlyAllStatistics(
+            @Auth AuthUser authUser) {
+
+        // AdminStoreServiceImpl의 getMonthlyAllStatistics 호출 (관리자만 가능)
+        StoreStatisticsResponseDto statistics = adminStoreService.getMonthlyAllStatistics(authUser.getEmail());
         return ResponseEntity.ok(statistics);
     }
 }
