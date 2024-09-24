0cf1ca589e1cc542409eba7b5df3d857b0ce9eca
20240729 <yoohyeonsik@gmail.com>
2024-09-24 19:16:42 +0900

    test: 자신의 주문 조회하기

--- src/test/java/com/nbacm/zzap_ki_yo/domain/order/service/OrderServiceImplTest.java
+++ src/test/java/com/nbacm/zzap_ki_yo/domain/order/service/OrderServiceImplTest.java
@@ -5,11 +5,13 @@ import com.nbacm.zzap_ki_yo.domain.menu.entity.Menu;
 import com.nbacm.zzap_ki_yo.domain.menu.repository.MenuRepository;
 import com.nbacm.zzap_ki_yo.domain.order.dto.OrderSaveRequest;
 import com.nbacm.zzap_ki_yo.domain.order.dto.OrderSaveResponse;
+import com.nbacm.zzap_ki_yo.domain.order.entity.Order;
 import com.nbacm.zzap_ki_yo.domain.order.repository.OrderRepository;
 import com.nbacm.zzap_ki_yo.domain.order.repository.OrderedMenuRepository;
 import com.nbacm.zzap_ki_yo.domain.store.entity.Store;
 import com.nbacm.zzap_ki_yo.domain.store.repository.StoreRepository;
 import com.nbacm.zzap_ki_yo.domain.user.entity.User;
+import com.nbacm.zzap_ki_yo.domain.user.entity.UserRole;
 import com.nbacm.zzap_ki_yo.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -47,6 +49,7 @@ class OrderServiceImplTest {
     @Mock
     private CouponRepository couponRepository;
 
+    Long userId = 1L;
     User user = new User();
     Long storeId = 1L;
     Store store = new Store();
@@ -94,8 +97,30 @@ class OrderServiceImplTest {
     @Test
     void getOrdersByUserAdmin() {
         //g
+        given(userRepository.findByEmailOrElseThrow(email)).willReturn(user);
+        given(userRepository.findById(1L)).willReturn(Optional.of(user));
+        Order order = new Order();
+        Order order2 = new Order();
+        Order order3 = new Order();
+        setField(order, "user", user);
+        setField(order2, "user", user);
+        setField(order3, "user", user);
+        setField(order, "store", store);
+        setField(order2, "store", store);
+        setField(order3, "store", store);
+        List<Order> orders = new ArrayList<>();
+        orders.add(order);
+        orders.add(order2);
+        orders.add(order3);
+        given(orderRepository.findAllByUserId(1L)).willReturn(orders);
+        List<Long> menuIds = new ArrayList<>();
+        menuIds.add(1L);
+        given(orderedMenuRepository.findMenuIdsByOrder(order)).willReturn(menuIds);
+        setField(user, "userRole", UserRole.USER);
         //w
+        List<OrderSaveResponse> responseList = orderService.getOrdersByUserAdmin(email, userId);
         //t
+        assertNotNull(responseList);
     }
 
     @Test
