47dadcaca7ce7bdb3875296dba9df5559fbed16b
lklll1212 <lklll1212@gmail.com>
2024-09-24 21:45:47 +0900

    [test] git add .

--- src/test/java/com/nbacm/zzap_ki_yo/Review/ReviewControllerTest.java
+++ src/test/java/com/nbacm/zzap_ki_yo/Review/ReviewControllerTest.java
@@ -1,10 +1,26 @@
 package com.nbacm.zzap_ki_yo.Review;
 
+import com.nbacm.zzap_ki_yo.domain.review.controller.ReviewController;
+import com.nbacm.zzap_ki_yo.domain.review.dto.ReviewSaveRequestDto;
 import com.nbacm.zzap_ki_yo.domain.review.service.ReviewService;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
 public class ReviewControllerTest {
 
     @Mock
     private ReviewService reviewService;
+
+    @InjectMocks
+    private ReviewController reviewController;
+
+
+    @Test
+    void saveReviewTest(){
+
+        //Given
+        String email = "test@example.com";
+        ReviewSaveRequestDto requestDto = new ReviewSaveRequestDto("맛있게 잘먹었습니다.",4);
+    }
 }
