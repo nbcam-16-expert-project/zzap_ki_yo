e80150f4fe8fa96e361bcaa2c3737f6c21d681dd
20240729 <yoohyeonsik@gmail.com>
2024-09-24 11:25:36 +0900

    test: 쿠폰 생성 사장이 아닐 때 예외

--- src/test/java/com/nbacm/zzap_ki_yo/domain/coupon/service/CouponServiceImplTest.java
+++ src/test/java/com/nbacm/zzap_ki_yo/domain/coupon/service/CouponServiceImplTest.java
@@ -2,10 +2,12 @@ package com.nbacm.zzap_ki_yo.domain.coupon.service;
 
 import com.nbacm.zzap_ki_yo.domain.coupon.dto.CouponRequest;
 import com.nbacm.zzap_ki_yo.domain.coupon.dto.CouponResponse;
+import com.nbacm.zzap_ki_yo.domain.coupon.exception.CouponForbiddenException;
 import com.nbacm.zzap_ki_yo.domain.coupon.repository.CouponRepository;
 import com.nbacm.zzap_ki_yo.domain.store.entity.Store;
 import com.nbacm.zzap_ki_yo.domain.store.repository.StoreRepository;
 import com.nbacm.zzap_ki_yo.domain.user.entity.User;
+import com.nbacm.zzap_ki_yo.domain.user.entity.UserRole;
 import com.nbacm.zzap_ki_yo.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -70,6 +72,56 @@ class CouponServiceImplTest {
 
         // then
         assertNotNull(couponResponse);
+    }
+
+    @Test
+    void 발행자가해당가게의사장이맞는지확인(){
+        //g
+        Long storeId = 1L;
+
+        CouponRequest couponRequest = new CouponRequest();
+        setField(couponRequest, "couponName", "Test Coupon");
+        setField(couponRequest, "discountRate", 20);
+        setField(couponRequest, "minPrice", 1000);
+        setField(couponRequest, "maxDiscount", 5000);
+        setField(couponRequest, "userId", 1L);
+        setField(couponRequest, "expiryPeriod", 30);
+
+        String email = "test@test.com";
+
+
+        //이메일에서 얻어온 스토어아이디랑 바로 받은 스토어아이디랑 달라야함
+        User publisher = new User();
+        setField(publisher, "userRole", UserRole.OWNER);
+        setField(publisher, "email", email);
+        List<Store> stores = new ArrayList<>();
+        Store ownedStore = new Store();
+        setField(ownedStore, "storeId", 2L);
+        stores.add(ownedStore);
+        setField(publisher, "stores", stores);
+
+
+
+        User user = new User();
+
+        Store serchedStore = new Store();
+        setField(serchedStore, "storeId", storeId);
+        // 유저 - 그냥 유저
+        // 퍼블리셔 - 2번가게 소유, 제공 이메일을 갖고있음, 유저롤 오너
+        // 발행할 가게 - 1번, 제공된 아이디를 갖고있음
+
+
+
+        given(userRepository.findByEmailOrElseThrow(email)).willReturn(publisher);
+        given(userRepository.findById(couponRequest.getUserId())).willReturn(Optional.of(user));
+        given(storeRepository.findById(storeId)).willReturn(Optional.of(serchedStore));
+
+        //w
+        CouponForbiddenException exception = assertThrows(CouponForbiddenException.class,
+                () -> service.saveCoupon(storeId, couponRequest, email));
+
+        //t
+        assertEquals("해당 가게의 소유자만 쿠폰을 발행할 수 있습니다.",exception.getMessage());
 
     }
 }
