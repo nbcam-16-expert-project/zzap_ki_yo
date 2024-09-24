6ebeed8dfd1fd52931e6a6a650173b8b99c15848
lklll1212 <lklll1212@gmail.com>
2024-09-24 21:47:04 +0900

    [fix] storeResponseDto

--- src/main/java/com/nbacm/zzap_ki_yo/domain/store/dto/response/SelectAllStoreResponseDto.java
+++ src/main/java/com/nbacm/zzap_ki_yo/domain/store/dto/response/SelectAllStoreResponseDto.java
@@ -17,18 +17,21 @@ public class SelectAllStoreResponseDto {
     private String storeAddress;
     private String storeNumber;
     private StoreType storeType;
+    private String storeNotice;
     private Integer favoriteCount;
     private AdType adType;
 
 
+
     @Builder
     public SelectAllStoreResponseDto(Long storeId, String storeName, String storeAddress, String storeNumber,
-                                     StoreType storeType, Integer favoriteCount, AdType adType) {
+                                     StoreType storeType,String storeNotice, Integer favoriteCount, AdType adType) {
         this.storeId = storeId;
         this.storeName = storeName;
         this.storeAddress = storeAddress;
         this.storeNumber = storeNumber;
         this.storeType = storeType;
+        this.storeNotice =storeNotice;
         this.favoriteCount = favoriteCount;
         this.adType = adType;
     }
@@ -41,6 +44,7 @@ public class SelectAllStoreResponseDto {
                 .storeAddress(store.getStoreAddress())
                 .storeNumber(store.getStoreNumber())
                 .storeType(store.getStoreType())
+                .storeNotice(store.getStoreNotice())
                 .favoriteCount(store.getFavoriteCount())
                 .adType(store.getAdType())
                 .build();
--- src/main/java/com/nbacm/zzap_ki_yo/domain/store/dto/response/SelectStoreResponseDto.java
+++ src/main/java/com/nbacm/zzap_ki_yo/domain/store/dto/response/SelectStoreResponseDto.java
@@ -15,17 +15,19 @@ public class SelectStoreResponseDto {
     private String storeName;
     private String storeAddress;
     private String storeNumber;
+    private String storeNotice;
     private Integer favoriteCount;
     private List<MenuNamePrice> menus = new ArrayList<>();
     private List<Category> categoryList;
 
 
     @Builder
-    private SelectStoreResponseDto(Long storeId, String storeName, String storeAddress, String storeNumber, List<MenuNamePrice> menus , Integer favoriteCount, List<Category> categoryList) {
+    private SelectStoreResponseDto(Long storeId, String storeName, String storeAddress, String storeNumber,String storeNotice, List<MenuNamePrice> menus , Integer favoriteCount, List<Category> categoryList) {
         this.storeId = storeId;
         this.storeName = storeName;
         this.storeAddress = storeAddress;
         this.storeNumber = storeNumber;
+        this.storeNotice = storeNotice;
         this.menus = menus;
         this.favoriteCount = favoriteCount;
         this.categoryList = categoryList;
@@ -37,6 +39,7 @@ public class SelectStoreResponseDto {
                 .storeName(store.getStoreName())
                 .storeAddress(store.getStoreAddress())
                 .storeNumber(store.getStoreNumber())
+                .storeNotice(store.getStoreNotice())
                 .favoriteCount(store.getFavoriteCount())
                 .menus(menus)
                 .categoryList(store.getCategoryList())
